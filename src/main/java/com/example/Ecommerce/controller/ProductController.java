02842ce85060c4c30adcf64f0e2cac0dfc350f2a
ankitaugale23 <ankitaugale23@gmail.com>
2021-12-17 17:39:40 +0530

    products added

--- src/main/java/com/example/Ecommerce/controller/ProductController.java
+++ src/main/java/com/example/Ecommerce/controller/ProductController.java
@@ -2,9 +2,12 @@ package com.example.Ecommerce.controller;
 
 import com.example.Ecommerce.model.Product;
 import com.example.Ecommerce.service.ProductService;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,16 +19,18 @@ public class ProductController {
     @Autowired
     ProductService service;
 
-    @GetMapping("/products/dairy")
-    public String showDairyProducts(){
-       List<String> products = service.retrieveByType(2);
+    @GetMapping("/products/dairy/{id}")
+    public String showDairyProducts(@PathVariable("id") int id, Model model){
+       List<String> products = service.retrieveByType(id);
+       model.addAttribute("dairyproducts",products);
        return "dairyproduct.html";
 
     }
 
-    @GetMapping("/products/farm")
-    public String showFarmProducts() {
-        List<String> products = service.retrieveByType(1);
+    @GetMapping("/products/farm/{id}")
+    public String showFarmProducts(@PathVariable("id") int id, Model model) {
+        List<String> products = service.retrieveByType(id);
+        model.addAttribute("farmproducts",products);
         return "farmproduct.html";
     }
 
